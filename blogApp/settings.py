36e92be58f30442c1c7646f309d77c0b48bb658a
Vincent Maximo II <projects.manager@xdomain.it>
2023-07-29 18:43:25 +0200

    setup vercel settings

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
